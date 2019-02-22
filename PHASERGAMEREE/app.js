c2dbebce379dcef8764479a90ce82b67f15e9aa8
Miregow <250011@hkis.edu.hk>
2019-02-22 18:15:04 +0800

    PHASERGAMEREE

--- PHASERGAMEREE/app.js
+++ PHASERGAMEREE/app.js
@@ -5,11 +5,11 @@ var score = 0;
 var lives = 3;
 
 function preload (){
-	game.load.image('sky','assets/sky.png')
-	game.load.image('star','assets/star.png')
-	game.load.image('aid','assets/firstaid.png')
-	game.load.image('diamond','assets/diamond.png')
-	game.load.image('platform','assets/platform.png')
+	game.load.image('sky','Assets/sky.png')
+	game.load.image('star','Assets/star.png')
+	game.load.image('aid','Assets/firstaid.png')
+	game.load.image('diamond','Assets/diamond.png')
+	game.load.image('platform','Assets/platform.png')
 
 	game.load.spritesheet('dude','assets/dude.png', 32, 48)
 	game.load.spritesheet('baddie','assets/baddie.png', 32, 32)
