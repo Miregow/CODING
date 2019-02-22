75596f69e4cae9fe7db79e6de71a8dabb9317850
Miregow <250011@hkis.edu.hk>
2019-02-22 18:10:42 +0800

    PHASERGAMEREE

--- PHASERGAMEREE/app.js
+++ PHASERGAMEREE/app.js
@@ -16,7 +16,7 @@ function preload (){
 }
 
 function create(){
-	game.physics.startSystem(Phaser.physics.ARCADE);
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.add.sprite(0,0,'sky')
 	platforms = games.add.physicsGroup();
 	platforms.enableBody = true;
