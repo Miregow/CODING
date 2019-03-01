4177d7ee1ab38a86b4ba2589d059bd21fa66a2b0
Miregow <250011@hkis.edu.hk>
2019-03-01 17:34:49 +0800

    lesson8

--- PHASERGAMEREE/app.js
+++ PHASERGAMEREE/app.js
@@ -11,8 +11,8 @@ function preload (){
 	game.load.image('diamond','Assets/diamond.png')
 	game.load.image('platform','Assets/platform.png')
 
-	game.load.spritesheet('dude','assets/dude.png', 32, 48)
-	game.load.spritesheet('baddie','assets/baddie.png', 32, 32)
+	game.load.spritesheet('dude','Assets/dude.png', 32, 48)
+	game.load.spritesheet('baddie','Assets/baddie.png', 32, 32)
 }
 
 function create(){
@@ -44,6 +44,58 @@ function create(){
 	livestext = game.add.text (120,5,score,style);
 	liveslabel.setShadow(3,3,'rgba(0,0,0,0.5)',2);
 	livestext.setShadow(3,3,'rgba(0,0,0,0.5)',2);
+
+player= game.add.sprite(32, 300, 'dude');
+	game.physics.arcade.enable(player)
+	player.body.gravity.y = 1000;
+	player.body.bounce.y = 0.5;
+	collide.body.collideWorldBounds = true;
+	player.animations.add('left', [0,1,2,3],12.5,true);
+	player.animations.add('right', [5,6,7,8],12.5,true);
+	
+
+enemy= game.add.sprite(760, 20, 'baddie');	
+game.physics.arcade.enable(player)
+	enemy.body.gravity.y = 1000;
+	enemy.body.bounce.y = 0.5;
+	collide.body.collideWorldBounds = true;
+	enemy.animations.add('left', [0,1],12.5,true);
+	enemy.animations.add('right', [2,3]12.5,true);
+
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	for(var i = 0; i<12; i++){
+		var star = stars.create(i*70,0,"star");
+		star.body.gravity = 200;
+		star.body.bounce.y = Math.random()*0.2+0.7;
+
+		wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+		aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+		sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+		dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	}
+
 }	
 
-function update(){}
+function update(){
+	game.physics.arcade.collide(player,platforms);
+	game.physics.arcade.collide(enemy,platforms);
+	game.physics.arcade.collide(stars,platforms);
+
+	player.body.velocity.x = 0;
+	
+	if(aKey.isDown){
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+	}else if(dKey.isDown){
+		player.body.velocity.x = 150;
+		player.animations.play('right');
+	}else{
+		player.animations.stop();
+		player.frame = 4;
+	}
+	if(wKey.isDown) && player.body.touching.down{
+		player.body.velocity.y = -300;
+
+	}
+}
